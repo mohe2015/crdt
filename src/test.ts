4242d37acfb9fb9e35ceb913ba18919e6e44a10a
mohe2015 <Moritz.Hedtke@t-online.de>
2020-12-29 23:21:51 +0100

    Revert github action checks annotation support

--- src/test.ts
+++ src/test.ts
@@ -23,157 +23,57 @@
  */
 import type { GrowOnlyCounter, GrowOnlySet,  Node } from "./index.js"
 import { addToGrowOnlySet, incrementGrowOnlyCounter, LastWriterWins, mergeGrowOnlySet, mergeLastWriterWins, mergeReplicatedCounter, updateLastWriterWins, valueOfReplicatedCounter } from "./index.js"
-import fs from 'fs/promises';
-import { findSourceMap } from 'module';
-import type { SourceMapping } from 'module'
-
-class MyError extends Error {
-    constructor(...args: any) {
-        super(...args)
-        Error.captureStackTrace(this, MyError)
-    }
-}
-
-function getStackInfo(stackElement: any): SourceMapping {
-    let sourceMap = findSourceMap(stackElement.getFileName())
-
-    if (sourceMap) {
-        return sourceMap.findEntry(stackElement.getLineNumber(), stackElement.getColumnNumber())
-    } else {
-        return {
-            generatedColumn: stackElement.getColumnNumber(),
-            generatedLine: stackElement.getLineNumber(),
-            originalColumn: stackElement.getColumnNumber(),
-            originalLine: stackElement.getLineNumber(),
-            originalSource: stackElement.getFileName()
-        }
-    }
-}
-
-function stackInfoToString(stackInfo: SourceMapping): string {
-    return getRelativeFileName(stackInfo.originalSource) + ":" + stackInfo.originalLine + ":" + stackInfo.originalColumn
-}
-
-function getRelativeFileName(absoluteFileName: string): string {
-    let index = absoluteFileName.indexOf("crdt/")
-    if (index !== -1) {
-        return absoluteFileName.substring(index+5)
-    } else {
-        return absoluteFileName
-    }
-}
-
-let jsonDelimiter = ""
 
 async function assertEqual(actual: any, expected: any) {
     if (actual !== expected) {
-        // https://v8.dev/docs/stack-trace-api
-
-        const _prepareStackTrace = Error.prepareStackTrace;
-        Error.prepareStackTrace = (_, stack) => stack;
-        const stack = new Error().stack!.slice(1) as unknown as any[];
-        Error.prepareStackTrace = _prepareStackTrace;
-
-        let stackInfo = getStackInfo(stack[0])
-        let value = JSON.stringify(
-            {
-                path: getRelativeFileName(stackInfo.originalSource),
-                start_line: stackInfo.originalLine - 1,
-                end_line: stackInfo.originalLine - 1,
-                start_column: stackInfo.originalColumn,
-                end_column: stackInfo.originalColumn,
-                annotation_level: "failure",
-                message: stack.map(getStackInfo).map(stackInfoToString).join("\n"),
-                title: actual + " !== " + expected,
-            }
-        , null, "\t")
-        console.log(value)
-        await fs.appendFile("./annotations.json", jsonDelimiter + value + "\n")
-        jsonDelimiter = ","
-
-        // https://docs.github.com/en/free-pro-team@latest/rest/reference/checks#annotations-items
-        throw new MyError(actual + " !== " + expected);
-    } else {
-        // https://v8.dev/docs/stack-trace-api
-
-        const _prepareStackTrace = Error.prepareStackTrace;
-        Error.prepareStackTrace = (_, stack) => stack;
-        const stack = new Error().stack!.slice(1) as unknown as any[];
-        Error.prepareStackTrace = _prepareStackTrace;
-
-        let stackInfo = getStackInfo(stack[0])
-        let value = JSON.stringify(
-            {
-                path: getRelativeFileName(stackInfo.originalSource),
-                start_line: stackInfo.originalLine - 1,
-                end_line: stackInfo.originalLine - 1,
-                start_column: stackInfo.originalColumn,
-                end_column: stackInfo.originalColumn,
-                annotation_level: "notice",
-                message: stack.map(getStackInfo).map(stackInfoToString).join("\n"),
-                title: actual + " === " + expected,
-            }
-        , null, "\t")
-        await fs.appendFile("./annotations.json", jsonDelimiter + value + "\n")
-        jsonDelimiter = ","
+        throw new Error(actual + " !== " + expected);
     }
 }
 
-try {
-    await fs.rm("./annotations.json", {
-        force: true
-    })
-    await fs.appendFile("./annotations.json", "[\n")
+let node1: Node = "node1"
+let counter1: GrowOnlyCounter = { [node1]: 2 }
+await assertEqual(valueOfReplicatedCounter(counter1), 2);
 
-    let node1: Node = "node1"
-    let counter1: GrowOnlyCounter = { [node1]: 2 }
-    await assertEqual(valueOfReplicatedCounter(counter1), 2);
+let node2: Node = "node2"
+let counter2: GrowOnlyCounter = incrementGrowOnlyCounter(counter1, node2, 10)
+await assertEqual(valueOfReplicatedCounter(counter2), 12)
 
-    let node2: Node = "node2"
-    let counter2: GrowOnlyCounter = incrementGrowOnlyCounter(counter1, node2, 10)
-    await assertEqual(valueOfReplicatedCounter(counter2), 12)
+let counter12 = mergeReplicatedCounter(counter1, counter2)
+await assertEqual(valueOfReplicatedCounter(counter12), 12)
 
-    let counter12 = mergeReplicatedCounter(counter1, counter2)
-    await assertEqual(valueOfReplicatedCounter(counter12), 12)
+let lww1: LastWriterWins = ["Technik", node1, { [node1]: 1 }]
+console.log("lww1", lww1)
 
-    let lww1: LastWriterWins = ["Technik", node1, { [node1]: 1 }]
-    console.log("lww1", lww1)
+let lww2 = updateLastWriterWins(lww1, node2, "Technik-AG")
+console.log("lww2", lww2)
 
-    let lww2 = updateLastWriterWins(lww1, node2, "Technik-AG")
-    console.log("lww2", lww2)
+let lww12 = mergeLastWriterWins(lww1, lww2)
+console.log("lww12", lww12)
 
-    let lww12 = mergeLastWriterWins(lww1, lww2)
-    console.log("lww12", lww12)
+let lww21 = mergeLastWriterWins(lww2, lww1)
+console.log("lww21", lww21)
 
-    let lww21 = mergeLastWriterWins(lww2, lww1)
-    console.log("lww21", lww21)
+await assertEqual(lww12, lww21)
 
-    await assertEqual(lww12, lww21)
+let lww211 = updateLastWriterWins(lww21, node1, "Technik")
+console.log("lww211", lww211)
 
-    let lww211 = updateLastWriterWins(lww21, node1, "Technik")
-    console.log("lww211", lww211)
+let lww212 = updateLastWriterWins(lww21, node2, "Technik AG")
+console.log("lww212", lww212)
 
-    let lww212 = updateLastWriterWins(lww21, node2, "Technik AG")
-    console.log("lww212", lww212)
-
-    let lww21x = mergeLastWriterWins(lww211, lww212)
-    console.log("lww21x", lww21x)
+let lww21x = mergeLastWriterWins(lww211, lww212)
+console.log("lww21x", lww21x)
 
 
-    await assertEqual(1, 4)
+await assertEqual(1, 4)
 
-    let gos: GrowOnlySet = [0, {}]
+let gos: GrowOnlySet = [0, {}]
 
-    let gos1 = addToGrowOnlySet(gos, node1, "node1eeee")
-    console.log(gos1)
+let gos1 = addToGrowOnlySet(gos, node1, "node1eeee")
+console.log(gos1)
 
-    let gos2 = addToGrowOnlySet(gos, node2, "node2eeeee")
-    console.log(gos2)
+let gos2 = addToGrowOnlySet(gos, node2, "node2eeeee")
+console.log(gos2)
 
-    let gosx = mergeGrowOnlySet(gos1, gos2)
-    console.log(gosx)
-} catch (error) {
-    console.log(error)
-} finally {
-    await fs.appendFile("./annotations.json", "]\n")
-}
+let gosx = mergeGrowOnlySet(gos1, gos2)
+console.log(gosx)
