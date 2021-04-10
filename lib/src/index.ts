aaa0b8acae3808e16a76b7bae581c32694ba5952
Hedtke, Moritz <Moritz.Hedtke@t-online.de>
2021-04-10 23:28:34 +0200

    Move to separate file

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -30,12 +30,10 @@
 import stringify from 'fast-json-stable-stringify';
 import {
   exportPublicKey,
-  generateKey,
   hashArrayBuffer,
   sign,
 } from './crypto';
 import crypto from '@dev.mohe/isomorphic-webcrypto';
-import type { JSONRPCHandler, JSONRPCRequest, JSONRPCResponse } from './json-rpc';
 import type { Remote } from './remote';
 
 // TODO use https://developer.mozilla.org/en-US/docs/Web/API/StorageManager/persist
@@ -51,12 +49,6 @@ export abstract class CmRDTTransaction<T> {
   abstract contains(hash: ArrayBuffer): Promise<boolean>
 }
 
-export async function hashObject<T>(object: T): Promise<ArrayBuffer> {
-  const stringified = stringify(object);
-  const enc = new TextEncoder();
-  return await crypto.subtle.digest('SHA-512', enc.encode(stringified));
-}
-
 export type CmRDTLogEntry<T> = Readonly<{
   value: T;
   hash: ArrayBuffer;
@@ -65,10 +57,6 @@ export type CmRDTLogEntry<T> = Readonly<{
   author: ArrayBuffer;
   signature: ArrayBuffer;
 }>;
-
-function cmrdtLogEntrySize<T>(entry: CmRDTLogEntry<T>): number {
-  return stringify(entry.value).length + entry.hash.byteLength + entry.random.byteLength + entry.previousHashes.map(a => a.byteLength).reduce((prev, curr) => prev + curr) + entry.author.byteLength + entry.signature.byteLength
-}
 
 export type CmRDTLog<T> = Readonly<CmRDTLogEntry<T>[]>;
 
@@ -139,129 +127,14 @@ export abstract class CmRDT<T> {
   }
 }
 
-export class IndexedDBCmRDTTransaction<T> extends CmRDTTransaction<T> {
-  transaction: IDBTransaction
-
-  constructor(transaction: IDBTransaction) {
-    super()
-    this.transaction = transaction
-  }
-
-  handleRequest<T>(request: IDBRequest<T>): Promise<T> {
-    return new Promise<T>((resolve, reject) => {
-      request.addEventListener('error', (event) => {
-        event.stopPropagation() // as the name implies this prevents propagation
-        reject(request.error)
-      })
-      request.addEventListener('success', () => {
-        resolve(request.result)
-      })
-    })
-  }
-
-  // NEW DATABASE DESIGN
-  // out of line auto-incrementing integer primary key | value | hash | previous | author | signature
-  // always store in topological order - this is hard with backwards insertion but would be benefical
-
-  /**
-   * @requires transaction be readwrite and accessing log and heads
-   * @param entries 
-   */
-  async insertEntries(entries: Set<CmRDTLogEntry<T>>): Promise<void> {
-    const logObjectStore = this.transaction.objectStore("log");
-    const headsObjectStore = this.transaction.objectStore("heads")
-
-    for (const entry of entries) {
-      await this.handleRequest(logObjectStore.add(entry));
-      await this.handleRequest(headsObjectStore.add(entry.hash))
-      await Promise.all(entry.previousHashes.map(h => this.handleRequest(headsObjectStore.delete(h))))
-    }
-  }
-
-  /**
-   * @requires transaction be readonly and accessing heads
-   * @returns 
-   */
-  async getHeads(): Promise<Set<ArrayBuffer>> {
-    const result = await this.handleRequest(this.transaction.objectStore("heads").getAllKeys());
-    return new Set(result as ArrayBuffer[])
-  }
-
-  /**
-   * @requires transaction be readonly and accessing log
-   * @param entries 
-   * @returns 
-   */
-  async getEntries(entries: Set<ArrayBuffer>): Promise<Set<CmRDTLogEntry<any>>> {
-    const logObjectStore = this.transaction.objectStore("log");
-    const result = await Promise.all([...entries].map(async hash => await this.handleRequest(logObjectStore.get(hash))))
-    return new Set(result)
-  }
-
-  /**
-   * @requires transaction be readonly and accessing log
-   * @param hash 
-   * @returns 
-   */
-  async contains(hash: ArrayBuffer): Promise<boolean> {
-    return (await this.handleRequest(this.transaction.objectStore("log").getKey(hash))) === undefined
-  }
+function cmrdtLogEntrySize<T>(entry: CmRDTLogEntry<T>): number {
+  return stringify(entry.value).length + entry.hash.byteLength + entry.random.byteLength + entry.previousHashes.map(a => a.byteLength).reduce((prev, curr) => prev + curr) + entry.author.byteLength + entry.signature.byteLength
 }
 
-export class IndexedDBCmRDTFactory implements CmRDTFactory {
-
-  initialize<T>(databaseName: string) {
-    return new Promise<IndexedDBCmRDT<T>>((resolve, reject) => {
-      const request = indexedDB.open(databaseName, 2)
-      request.addEventListener("upgradeneeded", () => {
-        request.result.createObjectStore("log", {
-          autoIncrement: false,
-          keyPath: "hash"
-        });
-        request.result.createObjectStore("heads", {
-          autoIncrement: false,
-          keyPath: ""
-        })
-      });
-      request.addEventListener("error", () => {
-        reject(request.error);
-      });
-      request.addEventListener("success", () => {
-        resolve(new IndexedDBCmRDT<T>(request.result));
-      })
-      request.addEventListener("blocked", () => {
-        // TODO FIXME
-        reject(new Error("database blocked"));
-      })
-    });
-  }
-}
-
-export class IndexedDBCmRDT<T> extends CmRDT<T> {
-  idbDatabase: IDBDatabase;
-
-  constructor(idbDatabase: IDBDatabase) {
-    super()
-    this.idbDatabase = idbDatabase;
-  }
-
-  getTransaction(storeNames: Iterable<string>, mode?: IDBTransactionMode): [CmRDTTransaction<T>, Promise<void>] {
-    const transaction = this.idbDatabase.transaction(storeNames, mode);
-    const done = new Promise<void>((resolve, reject) => {
-      transaction.addEventListener('abort', () => {
-        console.warn(transaction.error)
-        resolve() // aborting was likely done explicitly so this SHOULD be fine
-      })
-      transaction.addEventListener('complete', () => {
-        resolve()
-      })
-      transaction.addEventListener('error', (event) => {
-        event.stopPropagation()
-        reject(transaction.error)
-      })
-    })
-    return [new IndexedDBCmRDTTransaction<T>(transaction), done]
-  }
+export async function hashObject<T>(object: T): Promise<ArrayBuffer> {
+  const stringified = stringify(object);
+  const enc = new TextEncoder();
+  return await crypto.subtle.digest('SHA-512', enc.encode(stringified));
 }
 
 // vector clocks
