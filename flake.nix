2e2d9cc15ef4d2474a2c178e26b61a833e2b965c
Hedtke, Moritz <Moritz.Hedtke@t-online.de>
2021-05-08 17:48:06 +0200

    Add container for testing

--- flake.nix
+++ flake.nix
@@ -20,5 +20,32 @@
           pkgs.nodePackages.npm-check-updates
         ];
       };
+      # TODO FIXME can't set the timeout for imperative containers and this breaks
+
+      # sudo nixos-container create crdt-psql --flake .#crdt-postgresql
+      # sudo nixos-container start  crdt-psql
+      # sudo nixos-container stop   crdt-psql
+      # sudo nixos-container update crdt-psql --flake .#crdt-postgresql # maybe stop before?
+      # psql -h crdt-psql -U crdt
+      nixosConfigurations.crdt-postgresql = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ({ ... }: {
+            boot.isContainer = true;
+
+            networking.hostName = "crdt-psql";
+
+            services.postgresql = {
+              enable = true;
+              enableTCPIP = true; # ONLY use for containers that are protected from external networks
+              authentication = "hostnossl crdt crdt 10.233.1.1 255.255.255.255 pam";
+            };
+
+            networking.firewall.allowedTCPPorts = [ 5432 ];
+
+            system.stateVersion = "21.05";
+          })
+        ];
+      };
     };
 }
